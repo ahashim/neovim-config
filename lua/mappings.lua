eadc5dc8502654d4a3571b6e1b785d3b740536ba
Ahmed Hashim <ahashim@users.noreply.github.com>
2021-12-28 21:07:12 -0500

    adding more mappings

--- lua/mappings.lua
+++ lua/mappings.lua
@@ -29,3 +29,25 @@ map {'n', '<Leader>w', ':write<CR>'}
 
 -- quick close
 map {'n', '<Leader>q', ':q<CR>'}
+
+-- clear search highlighting with Esc
+map {'n', '<Esc>', ':noh <CR>'}
+
+-- copy whole file content
+map {"n", "<C-a>", ":%y+ <CR>"} 
+
+-- new buffer
+map {"n", "<S-t>", ":enew <CR>"}
+
+-- new tab
+map {"n", "<C-t>b", ":tabnew <CR>"}
+
+-- toggle numbers
+map {"n", "<leader>n", ":set nu! <CR>"}
+
+-- Allow moving the cursor through wrapped lines with j, k, <Up> and <Down>
+-- http://www.reddit.com/r/vim/comments/2k4cbr/problem_with_gj_and_gk/
+map {"", "j", 'v:count || mode(1)[0:1] == "no" ? "j" : "gj"',  expr = true }
+map {"", "k", 'v:count || mode(1)[0:1] == "no" ? "k" : "gk"',  expr = true }
+map {"", "<Down>", 'v:count || mode(1)[0:1] == "no" ? "j" : "gj"',  expr = true }
+map {"", "<Up>", 'v:count || mode(1)[0:1] == "no" ? "k" : "gk"',  expr = true }
