f9e5fbcc60d3d0d1b5d8ad64225f606109ebffcc
Ahmed Hashim <ahashim@users.noreply.github.com>
2022-01-04 15:50:38 -0500

    adding indent-blankline settings

---
+++ lua/settings/indent-blankline.lua
@@ -0,0 +1,18 @@
+require('indent_blankline').setup {
+  buftype_exclude = { 'terminal' },
+  char = '▏',
+  filetype_exclude = {
+     'help',
+     'terminal',
+     'dashboard',
+     'packer',
+     'lspinfo',
+     'TelescopePrompt',
+     'TelescopeResults',
+     '',
+  },
+  indentLine_enabled = 1,
+  show_first_indent_level = false,
+  show_trailing_blankline_indent = false,
+}
+
